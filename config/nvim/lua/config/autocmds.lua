7e13ad5f565a62b850198a884c05278cf0891f8b
WayneNoschang <43730526+WayneNoschang@users.noreply.github.com>
2024-08-14 23:10:16 -0400

    Clean up nvim autocommands and change dashboard logo.

--- config/nvim/lua/config/autocmds.lua
+++ config/nvim/lua/config/autocmds.lua
@@ -7,12 +7,12 @@ local function augroup(name)
 end
 
 vim.api.nvim_create_autocmd({ "VimEnter", "VimResume" }, {
-  group = augroup("restore_vim_cursor"),
+  group = augroup("restore_nvim_cursor"),
   command = "set guicursor=n-v-c-sm:block-Cursor/lCursor,i-ci-ve:ver25-Cursor/lCursor,r-cr-o:hor20-Cursor/lCursor",
 })
 
 vim.api.nvim_create_autocmd({ "VimLeave", "VimSuspend" }, {
-  group = augroup("restore_terminal_cursor"),
+  group = augroup("restore_iterm_cursor"),
   command = "set guicursor=a:ver25",
 })
 
@@ -20,14 +20,5 @@ vim.api.nvim_create_autocmd({ "VimLeave", "VimSuspend" }, {
 See:
 `help lua-guide-autocommands`
 `help guicursor`
-
-
-`help faq` > CURSOR STYLE ISN'T RESTORED AFTER EXITING OR SUSPENDING AND RESUMING NVIM
-Terminals do not provide a way to query the cursor style. Use autocommands to
-manage the cursor style:
-    au VimEnter,VimResume * set guicursor=n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50
-      \,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor
-      \,sm:block-blinkwait175-blinkoff150-blinkon175
-
-    au VimLeave,VimSuspend * set guicursor=a:block-blinkon0
+`help faq` search for "CURSOR STYLE ISN'T RESTORED AFTER EXITING"
 --]]
