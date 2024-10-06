3c68eda6f1c288e570b548b294555ee297311d91
WayneNoschang <43730526+WayneNoschang@users.noreply.github.com>
2024-10-05 21:10:53 -0400

    Add aliases to neovim config directories

--- zsh/plugins/nvim.plugin.zsh
+++ zsh/plugins/nvim.plugin.zsh
@@ -1,19 +1,31 @@
 # neovim > hyperextensible Vim-based text editor
 # https://neovim.io/
 
+# --- Run Commands ---
+
+if [[ ! -L $XDG_CONFIG_HOME/nvim-lazyvim ]]; then
+  ln -s $HOME/Projects/lua/nvim/lazyvim $XDG_CONFIG_HOME/nvim-lazyvim
+fi
+
+if [[ ! -L $XDG_CONFIG_HOME/nvim-kickstart ]]; then
+  ln -s $HOME/Projects/lua/nvim/kickstart $XDG_CONFIG_HOME/nvim-kickstart
+fi
+
+
 
 # --- Aliases ---
 
-alias vim="nvim"
-
-# Aliases for using kickstart project config in neovim 
+# https://michaeluloth.com/neovim-switch-configs/
+alias vim="NVIM_APPNAME='nvim-lazyvim' nvim"
+alias nvim="NVIM_APPNAME='nvim-lazyvim' nvim"
+alias nvim-lazyvim="NVIM_APPNAME='nvim-lazyvim' nvim"
 alias nvim-kickstart="NVIM_APPNAME='nvim-kickstart' nvim"
-alias vim-kickstart="nvim-kickstart"
 
 
 
 # --- Parameters ---
 
+# export NVIM_APPNAME=".config-nvim-dir"
 export EDITOR="nvim"
 export MANWIDTH=100
-export MANPAGER="nvim +Man!"
+export MANPAGER="NVIM_APPNAME='nvim-lazyvim' nvim +Man!"
